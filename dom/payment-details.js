0a2fd6f54c91f072b8c3e55eb828f5f13830ff01
Arif14377 <arifrahman14377@gmail.com>
2026-01-22 11:03:07 +0700

    feat: create DOM cart item section in payment-details page

--- dom/payment-details.js
+++ dom/payment-details.js
@@ -1,64 +1,79 @@
+const data = JSON.parse(window.localStorage.getItem("cart"))
+
+
 const items = document.querySelector(".items")
 
-const item = document.createElement("div")
-item.classList.add("item")
-items.append(item)
+if (data) {
+    const item = document.createElement("div")
+    item.classList.add("item")
+    items.append(item)
 
-const divWrapperDescPayment = document.createElement("div")
-item.append(divWrapperDescPayment)
+    const divWrapperDescPayment = document.createElement("div")
+    item.append(divWrapperDescPayment)
 
-const imgProduct = document.createElement("img")
-imgProduct.setAttribute("src", "/assets/images/image 31.png")
-imgProduct.setAttribute("alt", "product")
-divWrapperDescPayment.append(imgProduct)
+    const imgProduct = document.createElement("img")
+    imgProduct.setAttribute("src", "/assets/images/image 31.png")
+    imgProduct.setAttribute("alt", "product")
+    divWrapperDescPayment.append(imgProduct)
 
-const descPaymentItem = document.createElement("div")
-descPaymentItem.classList.add("desc-payment-item")
-divWrapperDescPayment.append(descPaymentItem)
+    const descPaymentItem = document.createElement("div")
+    descPaymentItem.classList.add("desc-payment-item")
+    divWrapperDescPayment.append(descPaymentItem)
 
-const flashSale = document.createElement("span")
-flashSale.textContent = "FLASH SALE"
-descPaymentItem.append(flashSale)
+    const flashSale = document.createElement("span")
+    flashSale.textContent = "FLASH SALE"
+    descPaymentItem.append(flashSale)
 
-const productName = document.createElement("h3")
-productName.textContent = "Hazelnut Latte"
-descPaymentItem.append(productName)
+    const productName = document.createElement("h3")
+    productName.textContent = "Hazelnut Latte"
+    descPaymentItem.append(productName)
 
-const variant = document.createElement("p")
-descPaymentItem.append(variant)
+    const variant = document.createElement("p")
+    descPaymentItem.append(variant)
 
-const pcs = document.createElement("span")
-pcs.textContent = "2pcs"
-variant.append(pcs)
+    const pcs = document.createElement("span")
+    pcs.textContent = "2pcs"
+    variant.append(pcs)
 
-const size = document.createElement("span")
-size.textContent = "Regular"
-variant.append(size)
+    variant.append(" | ")
 
-const hotIce = document.createElement("span")
-hotIce.textContent = "Ice"
-variant.append(hotIce)
+    const size = document.createElement("span")
+    size.textContent = "Regular"
+    variant.append(size)
 
-const place = document.createElement("span")
-place.textContent = "Dine In"
-variant.append(place)
+    variant.append(" | ")
 
-const priceItem = document.createElement("p")
-descPaymentItem.append(priceItem)
+    const hotIce = document.createElement("span")
+    hotIce.textContent = "Ice"
+    variant.append(hotIce)
 
-const priceOri = document.createElement("span")
-priceOri.textContent = "IDR40.000"
-priceItem.append(priceOri)
-priceItem.append(priceItem.textContent = "IDR20.000")
+    variant.append(" | ")
 
+    const place = document.createElement("span")
+    place.textContent = "Dine In"
+    variant.append(place)
 
-const priceSale = document.createElement("")
+    const priceItem = document.createElement("p")
+    descPaymentItem.append(priceItem)
 
-const removeBtn = document.createElement("button")
-item.append(removeBtn)
+    const priceOri = document.createElement("span")
+    priceOri.textContent = "IDR40.000"
+    priceItem.append(priceOri)
 
-const imgRmBtn = document.createElement("img")
-imgRmBtn.classList.add("remove")
-imgRmBtn.setAttribute("src", "/assets/images/XCircle.svg")
-imgRmBtn.setAttribute("alt", "remove")
-removeBtn.append(imgRmBtn)
+    priceItem.append("IDR20.000")
+
+    const removeBtn = document.createElement("button")
+    item.append(removeBtn)
+
+    const imgRmBtn = document.createElement("img")
+    imgRmBtn.classList.add("remove")
+    imgRmBtn.setAttribute("src", "/assets/images/XCircle.svg")
+    imgRmBtn.setAttribute("alt", "remove")
+    removeBtn.append(imgRmBtn)
+} else {
+    const kosong = document.createElement("p")
+    kosong.textContent = "Keranjang kosong."
+    items.append(kosong)
+}
+
+// Calculate
